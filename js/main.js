8b2b3c737e2a69d8e10d214ec881d59f390d6d9a
iLoyola <ivan@iloyola.com>
2016-02-26 19:29:08 -0500

    update console log

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ $( document ).ready( function() {
 
   var marvelAPI = 'https://gateway.marvel.com/v1/public/characters';
   $.getJSON( marvelAPI, {
-    apikey: '1024760f96a058cf6804cd870f6fdc97',
     ts: '1',
-    hash: '7c402660a1b4308d0136dc1f764c156de312db95'
+    hash: '7c402660a1b4308d0136dc1f764c156de312db95',
+    apikey: '1024760f96a058cf6804cd870f6fdc97'
   })
     .done( function( response ) {
       var results = response.data.results;
       var resultsLen = results.length;
       var output = '<ul>';
 
-      console.log( results[0] );
+      console.log( results );
 
       for( var i=0; i<resultsLen; i++ ){
         if( results[i].thumbnail.length > 0 ) {

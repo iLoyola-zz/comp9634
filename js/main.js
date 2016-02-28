82c7d276c9e054d8b977f42fe2bd5a1a5cad1a53
iLoyola <ivan@iloyola.com>
2016-02-28 11:50:48 -0500

    Adds js hooks in template and script

--- js/main.js
+++ js/main.js
@@ -2,25 +2,24 @@ $( document ).ready( function() {
 
   var marvelAPI = 'https://gateway.marvel.com/v1/public/characters';
   $.getJSON( marvelAPI, {
-    ts: '1',
-    hash: '7c402660a1b4308d0136dc1f764c156de312db95',
     apikey: '1024760f96a058cf6804cd870f6fdc97'
   })
-    .done( function( response ) {
-      var results = response.data.results;
-      var resultsLen = results.length;
-      var output = '<ul>';
+  .done( function( response ) {
+    var results = response.data.results;
+    var resultsLen = results.length;
+    var output = '<ul class="final-list">';
 
-      console.log( results );
+    console.log( results );
 
-      for( var i=0; i<resultsLen; i++ ){
-        if( results[i].thumbnail.length > 0 ) {
-          var imgPath = results[i].thumbnail[0].path + '/standard_xlarge.' + results[i].thumbnail[0].extension;
-          output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
-        }
+    for(var i=0; i<resultsLen; i++){
+      if(results[i].thumbnail.length > 0) {
+        var imgPath = results[i].thumbnail[0].path + '/standard_large.' + results[i].thumbnail[0].extension;
+        output += '<li class="final-list-item"><img src="' + imgPath + '"><h3 class="final-list-item-heading">'+results[i].name+'</h3><p class="final-list-item-text">' + results[i].description + '</p></li>';
+
       }
-      output += '</ul>';
-      $( '#results' ).append( output );
+    }
+    output += '</ul>';
+    $( '.js-final-results' ).append( output );
   });
 
 });

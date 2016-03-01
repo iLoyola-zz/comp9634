f20d7ecb2bb3755971999c6951ddf0092510ab94
iLoyola <ivan@iloyola.com>
2016-02-29 19:36:22 -0500

    Fixes modal main images

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ $( document ).ready( function() {
 
     var results = response.data.results;
     var resultsLen = results.length;
-    var output = '<ul class="final-results-list">';
     var body = $( 'body' );
+    var outputGrid = '<ul class="final-results-list">';
+    var outputModal = '<div class="final-modal-area">';
+    var closeButton = '<svg class="icon icon-cancel-circle js-icon-cancel-circle"><use xlink:href="#icon-cancel-circle"></use></svg>';
 
     var showInfo = function( event ) {
       event.stopPropagation();
@@ -23,22 +25,38 @@ $( document ).ready( function() {
 
     var openModal = function( event ) {
       event.stopPropagation();
-      body.find( '.js-final-modal' ).removeClass( 'hidden' );
       body.find( '.icon' ).css('display', 'block');
+      body.find( '.js-final-modal' ).removeClass( 'hidden' );
+      $( '.js-final-modal-meta' ).append( outputModal );
     };
 
-    for(var i=0; i<resultsLen; i++){
-      var imgPath = results[i].thumbnail.path + '/standard_large.' + results[i].thumbnail.extension;
-      output += '<li class="final-results-list-item"><button class="final-results-link js-final-results-link"><img src="' + imgPath + '"><div class="final-results-info hidden"><h3 class="final-results-heading">'+results[i].name+'</h3><p class="final-results-text">' + results[i].description + '</p></div></button></li>';
+    var closeModal = function( event ) {
+      body.find( '.js-final-modal' ).addClass( 'hidden' );
+    };
+
+    var leftModal = function( event ) {
+      event.stopPropagation();
+    };
+
+    var rightModal = function( event ) {
+      event.stopPropagation();
+    };
+
+    for( var i=0; i<resultsLen; i++ ){
+      var imgThumb = results[i].thumbnail.path + '/standard_large.' + results[i].thumbnail.extension;
+      var imgBig = results[i].thumbnail.path + '/landscape_incredible.' + results[i].thumbnail.extension;
+      outputModal += closeButton + '<img src="' + imgBig + '"><div class="final-modal-area-info"><h3 class="final-modal-area-heading">'+results[i].name+'</h3><p class="final-modal-area-text">' + results[i].description + '</p>';
+      outputGrid += '<li class="final-results-list-item"><button class="final-results-link js-final-results-link"><img src="' + imgThumb + '"><div class="final-results-info hidden"><h3 class="final-results-heading">'+results[i].name+'</h3><p class="final-results-text">' + results[i].description + '</p></div></button></li>';
     }
-    output += '</ul>';
+    outputGrid += '</ul>';
+    outputModal += '</div>';
 
-    $( '.js-final-results' ).append( output );
+    $( '.js-final-results' ).append( outputGrid );
 
-    $('.js-final-results-link').on( 'mouseenter', showInfo );
-    $('.js-final-results-link').on( 'mouseleave', hideInfo );
-    $('.js-final-results-link').on( 'click', openModal );
-
+    $( '.js-final-results-link' ).on( 'mouseenter', showInfo );
+    $( '.js-final-results-link' ).on( 'mouseleave', hideInfo );
+    $( '.js-final-results-link' ).on( 'click', openModal );
+    $( '.js-icon-cancel-circle' ).on( 'click', closeModal );
   });
 
 });

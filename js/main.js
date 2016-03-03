2b90c3a2ed725fa9ad13ce549e5f9fb856f96701
iLoyola <ivan@iloyola.com>
2016-03-02 20:51:43 -0500

    Fixes modal images

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $( document ).ready( function() {
 
   var marvelAPI = 'https://gateway.marvel.com/v1/public/characters';
   $.getJSON( marvelAPI, {
-    apikey: '1024760f96a058cf6804cd870f6fdc97'
+    apikey: '1024760f96a058cf6804cd870f6fdc97',
+    limit: '20',
+    offset: '1231'
   })
   .done( function( response ) {
 
@@ -10,8 +12,7 @@ $( document ).ready( function() {
     var resultsLen = results.length;
     var body = $( 'body' );
     var outputGrid = '<ul class="final-results-list">';
-    var outputModal = '<div class="final-modal-area">';
-    var closeButton = '<svg class="icon icon-cancel-circle js-icon-cancel-circle"><use xlink:href="#icon-cancel-circle"></use></svg>';
+    var closeButton = '<span class="final-modal-close js-final-modal-close"></span>';
 
     var showInfo = function( event ) {
       event.stopPropagation();
@@ -25,17 +26,24 @@ $( document ).ready( function() {
 
     var openModal = function( event ) {
       event.stopPropagation();
+      var currentData = $( this ).parent().data( 'current' );
+      var imgBig = results[currentData].thumbnail.path + '/landscape_incredible.' + results[currentData].thumbnail.extension;
       body.find( '.icon' ).css('display', 'block');
       body.find( '.js-final-modal' ).removeClass( 'hidden' );
-      $( '.js-final-modal-meta' ).append( outputModal );
+      $( '.js-final-modal-meta' ).html( '<div class="final-modal-area">' + closeButton + '<img src="' + imgBig + '"><div class="final-modal-area-info"><h3 class="final-modal-area-heading">'+results[currentData].name+'</h3><p class="final-modal-area-text">' + results[currentData].description + '</p></div>' );
+      $( '.final-modal-left' ).on( 'click', leftModal );
+      $( '.final-modal-right' ).on( 'click', rightModal );
     };
 
     var closeModal = function( event ) {
+      console.log( this );
+      body.find( '.icon' ).css('display', 'none');
       body.find( '.js-final-modal' ).addClass( 'hidden' );
     };
 
     var leftModal = function( event ) {
       event.stopPropagation();
+
     };
 
     var rightModal = function( event ) {
@@ -43,20 +51,21 @@ $( document ).ready( function() {
     };
 
     for( var i=0; i<resultsLen; i++ ){
+      var currentResult = i;
       var imgThumb = results[i].thumbnail.path + '/standard_large.' + results[i].thumbnail.extension;
-      var imgBig = results[i].thumbnail.path + '/landscape_incredible.' + results[i].thumbnail.extension;
-      outputModal += closeButton + '<img src="' + imgBig + '"><div class="final-modal-area-info"><h3 class="final-modal-area-heading">'+results[i].name+'</h3><p class="final-modal-area-text">' + results[i].description + '</p>';
-      outputGrid += '<li class="final-results-list-item"><button class="final-results-link js-final-results-link"><img src="' + imgThumb + '"><div class="final-results-info hidden"><h3 class="final-results-heading">'+results[i].name+'</h3><p class="final-results-text">' + results[i].description + '</p></div></button></li>';
+      outputGrid += '<li class="final-results-list-item" data-current="' + currentResult + '"><button class="final-results-link js-final-results-link"><img src="' + imgThumb + '"><div class="final-results-info hidden"><h3 class="final-results-heading">'+results[i].name+'</h3><p class="final-results-text">' + results[i].description + '</p></div></button></li>';
     }
     outputGrid += '</ul>';
-    outputModal += '</div>';
+
+
 
     $( '.js-final-results' ).append( outputGrid );
 
     $( '.js-final-results-link' ).on( 'mouseenter', showInfo );
     $( '.js-final-results-link' ).on( 'mouseleave', hideInfo );
     $( '.js-final-results-link' ).on( 'click', openModal );
-    $( '.js-icon-cancel-circle' ).on( 'click', closeModal );
+    $( '.js-final-modal-close' ).on( 'click', closeModal );
+    $().on( 'click', rightModal );
   });
 
 });
